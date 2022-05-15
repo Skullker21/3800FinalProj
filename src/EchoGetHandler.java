01f2655070f2773ee5df9a094a35e710f9815fda
Monroe <monroedrysdale@gmail.com>
2022-05-15 15:49:28 -0700

    Add JSON Handling

--- src/EchoGetHandler.java
+++ src/EchoGetHandler.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.json.simple.JSONObject;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
